1f632793cdca6f9b953997459f39803e2d8892fb
wanghaoran <adrianwhr0508@gmail.com>
2025-08-11 12:32:04 +0800

    这是一次在main上直接提交的

--- cmd/myapp/main.go
+++ cmd/myapp/main.go
@@ -2,6 +2,7 @@ package main
 
 import(
 
+	"strings"
 	"fmt"
 	"github.com/vipwhr/my-go-project/internal/pkg1"
 
@@ -9,6 +10,7 @@ import(
 
 func  main()  {
 	
+	fmt.Println(strings.TrimPrefix("http://zjuqx.cn:9014/drawings-backend/daily-paintings-full/22fe315f35ff692ea67d37f160c30601.jpg", "/"))
 	fmt.Println("My go project Started!!!!")
 	pkg1.HelloWorld()
 }
